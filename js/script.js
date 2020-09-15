3f4ad8ccc55ea0035cb57ec2bb4bd137817cde43
Zulfikharali Muzakir <zulfikar26error@gmail.com>
2020-09-15 22:47:02 +0700

    Fitur Search

--- js/script.js
+++ js/script.js
@@ -1,67 +1,66 @@
-var menuCafe = [
-{
-	img: "img/yakiniku.jpg",
-	namaMenu: "Yakiniku",
-	harga: "Rp 40.000"
-},
-{	
-	img: "img/sushi.jpg",
-	namaMenu: "Sushi",
-	harga: "Rp 28.000"
-},
-{
-	img: "img/onigiri.jpg",
-	namaMenu: "Onigiri",
-	harga: "Rp 10.000"
-},
-{
-	img: "img/greentea.jpg",
-	namaMenu: "Green Tea",
-	harga: "Rp 12.000"
-},
-{
-	img: "img/ramen.jpg",
-	namaMenu: "Ramen Noodles",
-	harga: "Rp 25.000"
-},
-{
-	img: "img/takoyaki.jpg",
-	namaMenu: "Takoyaki",
-	harga: "Rp 20.000"
-},
-{
-	img: "img/coffee.jpg",
-	namaMenu: "Coffee",
-	harga: "Rp 15.000"
-},
-{
-	img: "img/chicken-katsu.jpg",
-	namaMenu: "Chicken Katsu",
-	harga: "Rp 38.000"
-},
-{
-	img: "img/mochi.jpg",
-	namaMenu: "Mochi",
-	harga: "Rp 15.000"
-},
-{
-	img: "img/karaage.jpg",
-	namaMenu: "Karaage",
-	harga: "Rp 15.000"
-},
-{
-	img: "img/curry-rice.jpg",
-	namaMenu: "Curry Rice",
-	harga: "Rp 20.000",
-},
-{
-	img: "img/milktea.jpg",
-	namaMenu: "Milk Tea",
-	harga: "Rp 10.000"
-}
+var menuCafe = [{
+		img: "img/yakiniku.jpg",
+		namaMenu: "Yakiniku",
+		harga: "Rp 40.000"
+	},
+	{
+		img: "img/sushi.jpg",
+		namaMenu: "Sushi",
+		harga: "Rp 28.000"
+	},
+	{
+		img: "img/onigiri.jpg",
+		namaMenu: "Onigiri",
+		harga: "Rp 10.000"
+	},
+	{
+		img: "img/greentea.jpg",
+		namaMenu: "Green Tea",
+		harga: "Rp 12.000"
+	},
+	{
+		img: "img/ramen.jpg",
+		namaMenu: "Ramen Noodles",
+		harga: "Rp 25.000"
+	},
+	{
+		img: "img/takoyaki.jpg",
+		namaMenu: "Takoyaki",
+		harga: "Rp 20.000"
+	},
+	{
+		img: "img/coffee.jpg",
+		namaMenu: "Coffee",
+		harga: "Rp 15.000"
+	},
+	{
+		img: "img/chicken-katsu.jpg",
+		namaMenu: "Chicken Katsu",
+		harga: "Rp 38.000"
+	},
+	{
+		img: "img/mochi.jpg",
+		namaMenu: "Mochi",
+		harga: "Rp 15.000"
+	},
+	{
+		img: "img/karaage.jpg",
+		namaMenu: "Karaage",
+		harga: "Rp 15.000"
+	},
+	{
+		img: "img/curry-rice.jpg",
+		namaMenu: "Curry Rice",
+		harga: "Rp 20.000",
+	},
+	{
+		img: "img/milktea.jpg",
+		namaMenu: "Milk Tea",
+		harga: "Rp 10.000"
+	}
 ];
 
-menuCafe.map((menu, index)=> {
+var daftarMenu = (menu) => {
 	const daftarMenu = document.querySelector(".menunya");
 
 	daftarMenu.innerHTML += `<div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
@@ -73,4 +72,20 @@ menuCafe.map((menu, index)=> {
 									<p class="pt-1 text-gray-900">${menu.harga}</p>
 								</a>
 							</div>`;
-});
+}
+menuCafe.map(daftarMenu);
+
+//search data use filter
+var searchData = document.querySelector(".search");
+searchData.addEventListener('keyup', () => {
+		const search = menuCafe.filter((menu) => {
+			const input = document.querySelector(".search");
+			const namaMenu = menu.namaMenu.toLowerCase();
+			const typeInput = input.value.toLowerCase();
+
+			return namaMenu.includes(typeInput);
+		})
+
+		document.querySelector(".menunya").innerHTML = ``;
+		search.map(daftarMenu);
+	});
