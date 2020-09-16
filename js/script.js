43a8494d5b92b14e1705ae4ca356f40940e2b19c
Zulfikharali Muzakir <zulfikar26error@gmail.com>
2020-09-16 09:41:47 +0700

    Menambah Jenis Menu

--- js/script.js
+++ js/script.js
@@ -1,62 +1,74 @@
 var menuCafe = [{
 		img: "img/yakiniku.jpg",
 		namaMenu: "Yakiniku",
-		harga: "Rp 40.000"
+		harga: "Rp 40.000",
+		typeMenu: "Food"
 	},
 	{
 		img: "img/sushi.jpg",
 		namaMenu: "Sushi",
-		harga: "Rp 28.000"
+		harga: "Rp 28.000",
+		typeMenu: "Food"
 	},
 	{
 		img: "img/onigiri.jpg",
 		namaMenu: "Onigiri",
-		harga: "Rp 10.000"
+		harga: "Rp 10.000",
+		typeMenu: "Food"
 	},
 	{
 		img: "img/greentea.jpg",
 		namaMenu: "Green Tea",
-		harga: "Rp 12.000"
+		harga: "Rp 12.000",
+		typeMenu: "Drink"
 	},
 	{
 		img: "img/ramen.jpg",
 		namaMenu: "Ramen Noodles",
-		harga: "Rp 25.000"
+		harga: "Rp 25.000",
+		typeMenu: "Food"
 	},
 	{
 		img: "img/takoyaki.jpg",
 		namaMenu: "Takoyaki",
-		harga: "Rp 20.000"
+		harga: "Rp 20.000",
+		typeMenu: "Food"
 	},
 	{
 		img: "img/coffee.jpg",
 		namaMenu: "Coffee",
-		harga: "Rp 15.000"
+		harga: "Rp 15.000",
+		typeMenu: "Drink"
 	},
 	{
 		img: "img/chicken-katsu.jpg",
 		namaMenu: "Chicken Katsu",
-		harga: "Rp 38.000"
+		harga: "Rp 38.000",
+		typeMenu: "Food"
 	},
 	{
 		img: "img/mochi.jpg",
 		namaMenu: "Mochi",
-		harga: "Rp 15.000"
+		harga: "Rp 15.000",
+		typeMenu: "Food"
 	},
 	{
 		img: "img/karaage.jpg",
 		namaMenu: "Karaage",
-		harga: "Rp 15.000"
+		harga: "Rp 15.000",
+		typeMenu: "Food"
 	},
 	{
 		img: "img/curry-rice.jpg",
 		namaMenu: "Curry Rice",
 		harga: "Rp 20.000",
+		typeMenu: "Food"
 	},
 	{
 		img: "img/milktea.jpg",
 		namaMenu: "Milk Tea",
-		harga: "Rp 10.000"
+		harga: "Rp 10.000",
+		typeMenu: "Drink"
 	}
 ];
 
@@ -68,6 +80,7 @@ var daftarMenu = (menu) => {
 									<img class="hover:grow hover:shadow-lg" src="${menu.img}">
 									<div class="pt-3 flex items-center justify-between">
 										<p class="">${menu.namaMenu}</p>
+										<p class="type-menu">${menu.typeMenu}</p>
 									</div>
 									<p class="pt-1 text-gray-900">${menu.harga}</p>
 								</a>
@@ -78,14 +91,14 @@ menuCafe.map(daftarMenu);
 //search data use filter
 var searchData = document.querySelector(".search");
 searchData.addEventListener('keyup', () => {
-		const search = menuCafe.filter((menu) => {
-			const input = document.querySelector(".search");
-			const namaMenu = menu.namaMenu.toLowerCase();
-			const typeInput = input.value.toLowerCase();
+	const search = menuCafe.filter((menu) => {
+		const input = document.querySelector(".search");
+		const namaMenu = menu.namaMenu.toLowerCase();
+		const typeInput = input.value.toLowerCase();
 
-			return namaMenu.includes(typeInput);
-		})
+		return namaMenu.includes(typeInput);
+	})
 
-		document.querySelector(".menunya").innerHTML = ``;
-		search.map(daftarMenu);
-	});
+	document.querySelector(".menunya").innerHTML = ``;
+	search.map(daftarMenu);
+});
